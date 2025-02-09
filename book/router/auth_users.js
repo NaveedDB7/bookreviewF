99fa11780768e8d38fca415d1c1b856f82c37bd4
NaveedDB7 <naveednoushad100@gmail.com>
2025-02-09 21:23:04 +0530

    Update auth_users.js

--- book/router/auth_users.js
+++ book/router/auth_users.js
@@ -24,7 +24,19 @@ const isValid=(username)=>{
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+//code to check if username and password match the one we have in records.
+    const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 }
 
 //only registered users can login
